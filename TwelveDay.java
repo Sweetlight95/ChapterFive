2ade278d51ccabaa920915c56661b719a2030c2f
Sweetlight95 <princessadesuwa93@gmail.com>
2021-08-16 17:52:40 +0100

    chapter5

--- TwelveDay.java
+++ TwelveDay.java
@@ -4,32 +4,59 @@ import java.util.Scanner;
 
 public class TwelveDay {
         public static void main(String[] args) {
-            for (int count = 1; count <= 12; count++);
             Scanner song = new Scanner(System.in);
             System.out.println("Enter a number of your choice");
-            int songLyrics = song.nextInt();
-            switch (songLyrics) {
-                case 1: System.out.println("On the first day of Christmas my true love sent to me:");
-                break;
-                case 2: System.out.println("On the second day of Christmas my true love sent to me: 2 Turtle Doves");
-                break;
-//                case 3: System.out.println("");
-//                    break;
-//                case 3: System.out.println("");
-//                    break;
-//                    case 4: System.out.println("");
-//                    break;
-//                    case 5: System.out.println("");
-//                    break;
-//                    case 6: System.out.println("");
-//                    break;
-//                    case 7: System.out.println("");
-//                    break;
-//                    case 8: System.out.println("");
-//                    break;
-//                    case 9: System.out.println("");
-//                    break;
-               // switch (){}
+            int number = song.nextInt();
+            String songLyrics = "";
+            String daysOfChristmas = "";
+            String lyrics = "";
+            int count = 1;
+            number = 12;
+            while (count <= number) {
+                switch (count) {
+                    case 1 -> daysOfChristmas = "First";
+                    case 2 -> daysOfChristmas = "Second";
+                    case 3 -> daysOfChristmas = "Third";
+                    case 4 -> daysOfChristmas = "Fourth";
+                    case 5 -> daysOfChristmas = "Fifth";
+                    case 6 -> daysOfChristmas = "Sixth";
+                    case 7 -> daysOfChristmas = "Seventh";
+                    case 8 -> daysOfChristmas = "Eighth";
+                    case 9 -> daysOfChristmas = "Ninth";
+                    case 10 -> daysOfChristmas = "Tenth";
+                    case 11 -> daysOfChristmas = "Eleventh";
+                    case 12 -> daysOfChristmas = "Twelfth";
+                }
+                switch (count) {
+                    case 1: lyrics = "And a partridge in a pear tree.\n"; break;
+                    case 2: lyrics = "\nTwo turtle doves, \nAnd " + lyrics; break;
+                    case 3: lyrics = "\nThree French hens," + lyrics; break;
+                    case 4: lyrics = "\nFour calling birds," + lyrics; break;
+                    case 5: lyrics = "\nFive golden rings," + lyrics; break;
+                    case 6: lyrics = "\nSix geese a-laying," + lyrics; break;
+                    case 7: lyrics = "\nSeven swans a-swimming," + lyrics; break;
+                    case 8: lyrics = "\nEight maids a-milking," + lyrics; break;
+                    case 9: lyrics = "\nNine ladies dancing," + lyrics; break;
+                    case 10: lyrics = "\nTen lords a-leaping," + lyrics; break;
+                    case 11: lyrics = "\nEleven pipers piping," + lyrics; break;
+                    case 12: lyrics = "\nTwelve drummers drumming," + lyrics; break;
+
+                }
+                songLyrics = "\nOn the " + daysOfChristmas + " day of Christmas\n My true love gave to me: " + lyrics;
+                count++;
             }
+            System.out.println(songLyrics);
+            System.exit(count);
         }
 }
+
+
+
+
+
+
+
+
+
+
+
